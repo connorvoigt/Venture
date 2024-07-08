72f3a832c24f9d75104141def7e84b01e097aece
Daniel Gruen <d.ray.gruen@gmail.com>
2024-07-08 01:44:09 -0400

    Edited StayIn and Added Custom Fonts

--- src/pages/StayIn.js
+++ src/pages/StayIn.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../styles.css';
+// import '../styles.css';
+import '../stayIn.css';
 
 const StayIn = () => {
   const navigate = useNavigate();
@@ -12,43 +13,43 @@ const StayIn = () => {
   return (
     <div className="stay-in-container">
       <Link to="/" className="home-button">Home</Link>
+      <h1 className="title">Venture On... line!</h1>
       <div className="venture-box">
-        <h1>Venture On(line)!</h1>
         <div className="dropdown-container">
-          <div>
+          <div className="dropdown">
             <label htmlFor="setting">Setting</label>
-            <select id="setting">
-              <option value="">Select Setting</option>
+            <select id="setting" className="centered-dropdown">
+              <option value="">Select</option>
               <option value="home">Home</option>
               <option value="apartment">Apartment</option>
               <option value="dorm">Dorm</option>
               <option value="other">Other</option>
             </select>
           </div>
-          <div>
+          <div className="dropdown">
             <label htmlFor="current-mood">Current Mood</label>
-            <select id="current-mood">
-              <option value="">Select Current Mood</option>
+            <select id="current-mood" className="centered-dropdown">
+              <option value="">Select</option>
               <option value="happy">Happy</option>
               <option value="sad">Sad</option>
               <option value="relaxed">Relaxed</option>
               <option value="anxious">Anxious</option>
             </select>
           </div>
-          <div>
+          <div className="dropdown">
             <label htmlFor="desired-mood">Desired Mood</label>
-            <select id="desired-mood">
-              <option value="">Select Desired Mood</option>
+            <select id="desired-mood" className="centered-dropdown">
+              <option value="">Select</option>
               <option value="happy">Happy</option>
               <option value="sad">Sad</option>
               <option value="relaxed">Relaxed</option>
               <option value="anxious">Anxious</option>
             </select>
           </div>
-          <div>
+          <div className="dropdown">
             <label htmlFor="duration">Duration</label>
-            <select id="duration">
-              <option value="">Select Duration</option>
+            <select id="duration" className="centered-dropdown">
+              <option value="">Select</option>
               <option value="30">30 minutes</option>
               <option value="60">1 hour</option>
               <option value="90">1.5 hours</option>
@@ -57,6 +58,12 @@ const StayIn = () => {
           </div>
         </div>
         <button className="search-button" onClick={handleSearch}>Search!</button>
+      </div>
+      <div className="results-container">
+        <h2 className='results-title'>Results-</h2>
+        {/* <Result />
+        <Result />
+        <Result /> */}
       </div>
     </div>
   );
